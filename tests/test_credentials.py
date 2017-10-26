24a1e592db7aa9e653fd7bbb1bc4c17c41be4a9e
Doug Campbell <dcampbell@gogoair.com>
2017-10-26 13:27:18 -0500

    added unit tests for credentials

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -5,7 +5,18 @@ import pytest
 from artifactorypurge.exceptions import MissingEnvironmentVariable
 from artifactorypurge.credentials import load_credentials
 
-def test_missing_credentail_exception():
+def test_missing_credentials_exception():
     """Tests that an exception is raised if environment variable is missing"""
     with pytest.raises(MissingEnvironmentVariable):
         load_credentials()
+
+def test_load_credentials(monkeypatch):
+    """Tests that the right environment variables are loaded and returned"""
+    monkeypatch.setenv('ARTIFACTORY_URL', 'test_url')
+    monkeypatch.setenv('ARTIFACTORY_PASSWORD', 'test_password')
+    monkeypatch.setenv('ARTIFACTORY_USERNAME', 'test_username')
+    credentials = load_credentials()
+    assert credentials == {'artifactory_password': 'test_password',
+                           'artifactory_url': 'test_url',
+                           'artifactory_username': 'test_username'}
+    
