14d231d4ed294058cd94de60a582a3df91d2e5dd
Doug Campbell <dcampbell@gogoair.com>
2017-10-27 14:53:04 -0500

    added back in test that got nuked in a rebase

--- tests/test_setup_pluginbase.py
+++ tests/test_setup_pluginbase.py
@@ -16,3 +16,9 @@ def test_extra_policies_path_load(tmpdir):
     temp_policy.write(' ')
     plugin_source = setup_pluginbase(extra_policies_path=str(tmpdir))
     assert 'test_policy' in plugin_source.list_plugins()
+
+def test_bad_policies_path(tmpdir):
+    """Tests that an exception is raised for invalid path"""
+    invalid_path = str(tmpdir)+'/bad'
+    with pytest.raises(InvalidPoliciesDirectory):
+        plugin_source = setup_pluginbase(extra_policies_path=invalid_path)
