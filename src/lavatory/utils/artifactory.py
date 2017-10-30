5a08ece3e33163d568b5ecbec50ec765f0a0b27d
Bekzot Azimov <bazimov@gogoair.com>
2017-10-30 15:03:33 -0500

    Refactor and add docstrings.

--- src/lavatory/utils/artifactory.py
+++ src/lavatory/utils/artifactory.py
@@ -1,3 +1,4 @@
+"""Artifactory purger module."""
 import base64
 import datetime
 import logging
@@ -10,6 +11,7 @@ LOG = logging.getLogger(__name__)
 
 
 class Artifactory(object):
+    """Artifactory purger class."""
     def __init__(self, repo_name=None):
         self.repo_name = repo_name
         self.credentials = load_credentials()
@@ -22,14 +24,13 @@ class Artifactory(object):
 
     @staticmethod
     def _parse_artifact_name(name):
+        """Artifact name parser."""
         simple_name = '/'.join(name.split('/')[-4:])
         return simple_name
 
     def list(self):
         """
-        Return a list of repos with basic info about each
-        If the optional parameter repo_name is specified, then only return
-        information pertaining to that repo
+        Return a list of repos with basic info about each.
         """
 
         repos = {}
@@ -46,13 +47,18 @@ class Artifactory(object):
         return repos
 
     def all_artifacts(self, search='', depth=3):
-        """ Returns a dict of artifact and properties """
+        """ Returns a dict of artifact and properties.
+
+        Args:
+            search (str): Search regex.
+            depth (int): Depth.
+
+        Returns:
+            all_artifacts (dict): All artifacts.
+        """
         LOG.debug('Finding all artifacts with: search=%s, repo=%s, depth=%s', search, self.repo_name, depth)
 
-        all_artifacts = self.artifactory.find_by_pattern(
-            filename=search,
-            specific_repo=self.repo_name,
-            max_depth=depth)
+        all_artifacts = self.artifactory.find_by_pattern(filename=search, specific_repo=self.repo_name, max_depth=depth)
 
         for artifact in sorted(all_artifacts):
             artifact_simple_name = self._parse_artifact_name(artifact)
@@ -63,41 +69,43 @@ class Artifactory(object):
         return all_artifacts
 
     def purge(self, dry_run, artifacts):
-        """ Purge artifacts from the specified repo
+        """ Purge artifacts from the specified repo.
 
-        Keyword arguments:
-        repo -- the repo to target for this operation
-        dry_run -- false to execute an actual purge
-        artifacts -- a list of artifacts to operate upon
+        Args:
+            dry_run (bool): Dry run mode True/False
+            artifacts (list): Artifacts.
+
+        Returns:
+            purged (int): Count purged.
         """
         purged = 0
         mode = "DRYRUN" if dry_run else "LIVE"
 
         for artifact in artifacts:
-            LOG.info("  {} purge {}:{}".format(mode, self.repo_name, artifact))
+            LOG.info("%s purge %s:%s", (mode, self.repo_name, artifact))
             if dry_run:
                 purged += 1
             else:
                 try:
                     self.artifactory.request(artifact, method='delete')
                     purged += 1
-                except Exception as e:
-                    LOG.error(str(e))
+                except Exception as error:
+                    LOG.error(str(error))
 
         return purged
 
-    def filter(self, terms=None, depth=3, sort=None, offset=None, limit=None):
+    def filter(self, terms=None, depth=3):
         """Get a subset of artifacts from the specified repo.
 
         XXX: this looks at the project level, but actually need to iterate lower at project level
         XXX: almost certainly needs to set depth parameter to get to specific build
 
-        Keyword arguments:
-        repo -- the repo to target for this operation
-        terms -- an array of jql snippets that will be ANDed together
-        depth -- how far down the folder hierarchy to look
-        offset -- how many items from the beginning of the list should be skipped (optional)
-        limit -- the maximum number of entries to return (optional)
+        Args:
+            terms (list): AQL terms.
+            depth (int): Depth level.
+
+        Returns:
+            results: Results from aql search.
 
         This method does not use pagination. It assumes that this utility
         will be called on a repo sufficiently frequently that removing just
@@ -113,7 +121,7 @@ class Artifactory(object):
 
         aql = {"$and": terms}
 
-        LOG.debug("AQL: {}".format(aql))
+        LOG.debug("AQL: %s", aql)
 
         response = self.artifactory.find_by_aql(criteria=aql, fields=['stat'])
 
@@ -122,6 +130,18 @@ class Artifactory(object):
         return results
 
     def retain(self, spec_project, depth=3, terms=None, count=None, weeks=None):
+        """Returns purgable artifacts.
+
+        Args:
+            spec_project: Spec_project
+            depth (int): Depth level.
+            terms: Terms for filter.
+            count (int): Count.
+            weeks: Number of weeks.
+
+        Returns:
+            purgable (list): Purgable.
+        """
         if [terms, count, weeks].count(None) != 2:
             raise ValueError("Must specify exactly one of terms, count, or weeks")
 
@@ -133,23 +153,17 @@ class Artifactory(object):
 
             path = "{}/{}".format(project["path"], project["name"])
             if count:
-                for artifact in self.filter(
-                        offset=count, depth=depth + 1, terms=[{
-                            "path": path
-                        }], sort={"$desc": ["created"]}):
+                filtered = self.filter(depth=depth + 1, terms=[{"path": path}])
+
+                for artifact in filtered:
                     purgable.append("{}/{}".format(artifact["path"], artifact["name"]))
 
             if weeks:
                 now = datetime.datetime.now()
                 before = now - datetime.timedelta(weeks=weeks)
                 created = before.strftime("%Y-%m-%dT%H:%M:%SZ")
-
-                for artifact in self.filter(
-                        offset=count, depth=depth + 1, terms=[{
-                            "path": path
-                        }, {
-                            "created": created
-                        }]):
+                filtered = self.filter(depth=depth + 1, terms=[{"path": path}, {"created": created}])
+                for artifact in filtered:
                     purgable.append("{}/{}".format(artifact["path"], artifact["name"]))
 
             if terms:
