02d9961c4a6e9c3727b93df12cea9d345b6bc322
Doug Campbell <dcampbell@gogoair.com>
2017-11-02 09:54:09 -0500

    split purge command into 2 other functions for easier testing

--- src/lavatory/commands/purge.py
+++ src/lavatory/commands/purge.py
@@ -15,19 +15,35 @@ LOG = logging.getLogger(__name__)
 @click.option(
     '--dryrun/--nodryrun', default=True, is_flag=True, help='Dryrun does not delete any artifacts. On by default')
 @click.option('--default/--no-default', default=True, is_flag=True, help='If false, does not apply default policy')
-@click.option('--repo', default=None, multiple=True, required=False, 
-              help='Name of specific repository to run against. Can use --repo multiple times. If not provided, uses all repos.')
+@click.option(
+    '--repo',
+    default=None,
+    multiple=True,
+    required=False,
+    help='Name of specific repository to run against. Can use --repo multiple times. If not provided, uses all repos.')
 def purge(ctx, dryrun, policies_path, default, repo):
     """Deletes artifacts based on retention policies"""
     artifactory = Artifactory(repo_name=None)
-    plugin_source = setup_pluginbase(extra_policies_path=policies_path)
-
     before_repo_data = artifactory.list()
     if repo:
         all_repos = repo
     else:
-        all_repos = before_repo_data.keys()
-    
+        all_repos = before_purge_data.keys()
+
+    apply_purge_policies(all_repos, dryrun=dryrun)
+    generate_purge_report(all_repos, before_purge_data)
+
+    LOG.info("Success.")
+
+
+def apply_repo_policy(all_repos, dryrun=True):
+    """Sets up the plugins to find purgable artifacts and delete them. 
+
+    Args:
+        all_repos (list): List of repos to run against.
+        dryrun (bool): If true, will not actually delete artifacts.
+    """
+    plugin_source = setup_pluginbase(extra_policies_path=policies_path)
     LOG.info("Applying retention policies to %s", ', '.join(all_repos))
     for repo in all_repos:
         policy_name = repo.replace("-", "_")
@@ -41,20 +57,24 @@ def purge(ctx, dryrun, policies_path, default, repo):
             else:
                 LOG.info("No policy found for %s. Skipping Default", repo)
                 continue
-        artifacts = policy.purgelist(
-            artifactory_repo
-        )
-        count = artifactory_repo.purge(dryrun, artifacts)
-        LOG.info("Processed {}, Purged {}".format(repo, count))
+        artifacts = policy.purgelist(artifactory_repo)
+        purged_count = artifactory_repo.purge(dryrun, artifacts)
+        LOG.info("Processed {}, Purged {}".format(repo, purged_count))
 
-    LOG.info("")
-    LOG.info("Purging Performance:")
-    after_repo_data = artifactory.list()
-    for repo, info in after_repo_data.items():
-        if repo in all_repos:
+
+def generate_purge_report(purged_repos, before_purge_data):
+    """Generates a performance report based on deleted artifacts.
+
+    Args:
+        purged_repos (list): List of repos that had policy applied.
+        before_purge_data (dict): Data on the state of Artifactory before purged artifacts
+    """
+    LOG.info("\nPurging Performance:")
+    artifactory = Artifactory(repo_name=None)
+    after_purge_data = artifactory.list()
+    for repo, info in after_purge_data.items():
+        if repo in purged_repos:
             try:
-                get_performance_report(repo, before_repo_data[repo], info)
+                get_performance_report(repo, before_purge_data[repo], info)
             except IndexError:
                 pass
-
-    LOG.info("Done.")
