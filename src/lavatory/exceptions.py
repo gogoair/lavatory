5d87a1785e8d0056f7c34ed9f89e15fb46d20f1c
Joel Vasallo <joelvasallo@gmail.com>
2018-10-16 05:00:12 +0000

    fix: Typo in Exception (Environment Variable)

--- src/lavatory/exceptions.py
+++ src/lavatory/exceptions.py
@@ -9,7 +9,7 @@ class MissingEnvironmentVariable(LavatoryError):
     """Required environment variable is missing"""
 
     def __init__(self, missing_var):
-        error = 'Missing Environement variable {0}.'.format(missing_var)
+        error = 'Missing Environment variable {0}.'.format(missing_var)
         super().__init__(error)
 
 
